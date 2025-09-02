89071d2880ba635cac2ecabae0a0be3825ccd8fa
Savio Oliveira <54548469+savio-dev@users.noreply.github.com>
2025-09-01 23:35:18 -0300

    Create build.gradle.kts

---
+++ AnimesRollProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "Plugin para o site AnimesROLL"
+    language = "pt"
+    authors = listOf("SeuNome")
+
+    // Nome que aparecerá no CloudStream
+    pluginName = "AnimesROLL"
+
+    tvTypes = listOf(
+        "Anime"
+    )
+
+    iconUrl = "https://www.anroll.net/favicon.ico"
+}
